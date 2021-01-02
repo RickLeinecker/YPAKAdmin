0439c76ba7f0b7881b2cb3c32684f5a2485588ab
Gonen Matias <gonen.matias@knights.ucf.edu>
2021-01-02 05:42:55 -0500

    Auth 'Context' from firestore (additional user data)

--- src/store/actions/authActions.js
+++ src/store/actions/authActions.js
@@ -5,11 +5,12 @@ import {
 	AUTH_LOGIN_FAILURE,
 	AUTH_LOGOUT_REQUEST,
 	AUTH_LOGOUT_SUCCESS,
-	AUTH_RESTORE,
+	AUTH_RESTORE_FAILURE,
+	AUTH_RESTORE_SUCCESS,
+	AUTH_RESTORE_REQUEST,
 	AUTH_FLUSH,
 } from './types/authTypes'
 
-export const authRestore = (auth) => ({ type: AUTH_RESTORE, auth });
 export const authFlush = () => ({ type: AUTH_FLUSH });
 
 const authLoginRequest = () => ({ type: AUTH_LOGIN_REQUEST });
@@ -35,7 +36,7 @@ const authLogoutRequest = () => ({ type: AUTH_LOGOUT_REQUEST });
 const authLogoutSuccess = () => ({ type: AUTH_LOGOUT_SUCCESS });
 export const authLogout = () => {
 	return (dispatch) => {
-		dispatch(authLoginRequest());
+		dispatch(authLogoutRequest());
 		firebase.auth().signOut()
 			.then(() => {
 				dispatch(authLogoutSuccess());
@@ -45,5 +46,44 @@ export const authLogout = () => {
 				console.log("Logout error", err);
 				dispatch(authLogoutSuccess()); // For now.
 			})
+	}
+}
+
+
+const authRestoreRequest = () => ({ type: AUTH_RESTORE_REQUEST });
+const authRestoreSuccess = (auth) => ({ type: AUTH_RESTORE_SUCCESS, auth });
+const authRestorFailure = (error) => ({ type: AUTH_RESTORE_FAILURE, error });
+export const authRestore = (auth) => {
+	return (dispatch) => {
+		// Verify basic auth data (uid)
+		if (!auth || !auth.uid)
+			return dispatch(authRestorFailure("Invalid user, please log in again."))
+
+		// Loading prompt
+		dispatch(authRestoreRequest());
+
+		// Fetch additional auth data
+		// firebase.firestore().collection("Users").doc("fail").get()
+		firebase.firestore().collection("Users").doc(auth.uid).get()
+			.then((doc) => {
+				const docData = doc.data();
+				
+				if (!doc || !doc.exists) 
+					throw Error('Invalid user, please log in again.');
+
+				// Append additional auth data
+				auth.groupId = docData.belongsTo.id;
+				auth.super = docData.super;
+				
+				// Success
+				dispatch(authRestoreSuccess(auth));
+			})
+			.catch((err) => {
+					console.log(err)	
+					console.log(err.message)	
+					firebase.auth().signOut()
+					.finally(() => dispatch(authRestorFailure(err.message)))
+				}
+			);
 	}
 }
--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -4,29 +4,40 @@ import {
 	AUTH_LOGIN_FAILURE,
 	AUTH_LOGOUT_REQUEST,
 	AUTH_LOGOUT_SUCCESS,
-	AUTH_RESTORE,
 	AUTH_FLUSH,
+	AUTH_RESTORE_SUCCESS,
+	AUTH_RESTORE_FAILURE,
+	AUTH_RESTORE_REQUEST,
 } from '../actions/types/authTypes';
 
 const initState = {
 	auth: undefined,
 	auth_loading: undefined,
 	auth_error: undefined,
+	auth_loaded: undefined,
 }
 
 const authReducer = (state = initState, action) => {
 
 	switch (action.type) {
-		case AUTH_LOGIN_FAILURE: return { ...initState, auth_error: action.error };
-		case AUTH_LOGIN_REQUEST: return { ...initState, auth_loading: true };
+		case AUTH_LOGIN_FAILURE:
+		case AUTH_RESTORE_FAILURE:
+			return { ...initState, auth_error: action.error };
+			
+		case AUTH_LOGIN_REQUEST:
+		case AUTH_RESTORE_REQUEST:
+			return { ...initState, auth_loading: true };
+
 		case AUTH_LOGIN_SUCCESS:
-		case AUTH_RESTORE:
 			return { ...initState, auth: action.auth }
+
+		case AUTH_RESTORE_SUCCESS:
+			return { ...initState, auth: action.auth, auth_loaded: true }
 
 		case AUTH_LOGOUT_REQUEST: console.log("LOGOUT Request is WIP..."); return state;
 		case AUTH_LOGOUT_SUCCESS:
 		case AUTH_FLUSH:
-			return { ...initState }
+			return { ...initState, auth_error: state.auth_error }
 
 		default: return state;
 	}
